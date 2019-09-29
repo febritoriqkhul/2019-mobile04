dea6c93004e473bcafe42ed50eaa5ccbb7ec49d4
febritoriqkhul <febritoriqkhul@gmail.com>
2019-09-29 07:51:19 +0700

    Android Bundle activity done.

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,8 +3,17 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static id.ac.polinema.intent.BundleActivity.AGE_KEY;
+import static id.ac.polinema.intent.BundleActivity.NAME_KEY;
+import static id.ac.polinema.intent.BundleActivity.USERNAME_KEY;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +21,21 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
+
         if (extras != null) {
             // TODO: display value here
+            String username = extras.getString(BundleActivity.USERNAME_KEY);
+            String name = extras.getString(BundleActivity.NAME_KEY);
+            int age = extras.getInt(BundleActivity.AGE_KEY);
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(String.valueOf(age));
         }
     }
 }
